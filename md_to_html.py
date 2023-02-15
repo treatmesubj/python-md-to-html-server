c104db458139701ab5cc6b8a5fd46e4224709d13
treatmesubj <jrock4503@hotmail.com>
2023-02-15 11:07:12 -0600

    md_to_html.py

---
+++ md_to_html.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from markdown_it import MarkdownIt
+# from markdown_it.presets import gfm_like
+from bs4 import BeautifulSoup, element
+from md_to_html_server import markdown_to_html
+
+
+if __name__ == "__main__":
+    # gfm_like.make(); MarkdownIt_obj = MarkdownIt("gfm-like")
+    MarkdownIt_obj = MarkdownIt("commonmark").enable("table").enable("strikethrough")
+    try:
+        in_file_path = sys.argv[1]
+        out_file_path = sys.argv[2]
+    except IndexError as e:
+        print("\nusage: python md_to_html.py in_file.md out_file.html")
+        raise e
+    markdown_to_html(MarkdownIt_obj, in_file_path, out_file_path)
--- md_to_html_server.py
+++ md_to_html_server.py
@@ -29,15 +29,15 @@ from markdown_it import MarkdownIt
 from bs4 import BeautifulSoup, element
 
 
-def markdown_to_html(MarkdownIt_obj, file_path):
-    text = open(file_path, "r").read()
+def markdown_to_html(MarkdownIt_obj, in_file_path, out_file_path="tmp.html"):
+    text = open(in_file_path, "r").read()
     tokens = MarkdownIt_obj.parse(text)
     html_text = MarkdownIt_obj.render(text)
     # pretty CSS
     soup = BeautifulSoup(html_text, 'html5lib') # adds <html>, <head>,  <body>
     soup.select_one('head').append(soup.new_tag("style"))
     soup.select_one("style").string="body { background-color: #272822; color: white; font-family: Courier; } a[href] { color: #66d9ef; } code { color: #ae81ff; background-color: #272b33; border-radius: 6px; }"
-    Path("./tmp.html").write_text(str(soup))
+    Path(out_file_path).write_text(str(soup))
 
 
 class md_to_html_SimpleHTTPRequestHandler(SimpleHTTPRequestHandler):
@@ -57,7 +57,7 @@ class md_to_html_SimpleHTTPRequestHandler(SimpleHTTPRequestHandler):
             finally:
                 f.close()
         try:
-            os.remove("./tmp.html")  # remove temp html file
+            os.remove("tmp.html")  # remove temp html file
         except FileNotFoundError:
             pass
 
